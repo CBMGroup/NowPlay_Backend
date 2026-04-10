288ec7f9ccee4559f8fd35feacc6287c16916672
kwesigasavings-hash <kwesigasavings@gmail.com>
2026-04-10 19:31:05 +0300

    feat: initialize Django project settings with PostgreSQL, CORS, and JWT authentication support

--- nowplay_backend/settings.py
+++ nowplay_backend/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS','').split(',')
 
-CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '').split(',')
+CSRF_TRUSTED_ORIGINS = [origin for origin in os.getenv('CSRF_TRUSTED_ORIGINS', '').split(',') if origin]
 # Application definition
 
 INSTALLED_APPS = [
