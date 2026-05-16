952a6ab6e8f022821a74c2e94b13e280165660f9
Wawomo <deniswawomola75@gmail.com>
2026-05-16 12:44:42 +0300

    feat: add models for podcasts, audiobooks, audio plays, and poems with corresponding admin configuration

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import User, Artist, Album, Track, Playlist, Like, Stream
+from .models import (
+    User, Artist, Album, Track, Playlist, Like, Stream,
+    Podcast, PodcastEpisode, AudioBook, AudioBookChapter,
+    AudioPlay, AudioPlayAct, Poem
+)
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
@@ -9,8 +13,8 @@ class UserAdmin(admin.ModelAdmin):
 
 @admin.register(Artist)
 class ArtistAdmin(admin.ModelAdmin):
-    list_display = ('name', 'is_verified', 'created_at')
-    list_filter = ('is_verified',)
+    list_display = ('name', 'creator_type', 'is_verified', 'created_at')
+    list_filter = ('creator_type', 'is_verified')
     search_fields = ('name',)
 
 @admin.register(Album)
@@ -23,6 +27,34 @@ class TrackAdmin(admin.ModelAdmin):
     list_display = ('title', 'artist_name', 'category', 'plays', 'created_at')
     list_filter = ('category',)
     search_fields = ('title', 'artist_name')
+
+@admin.register(Podcast)
+class PodcastAdmin(admin.ModelAdmin):
+    list_display = ('title', 'host', 'created_at')
+
+@admin.register(PodcastEpisode)
+class PodcastEpisodeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'podcast_series', 'episode_number')
+
+@admin.register(AudioBook)
+class AudioBookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_at')
+
+@admin.register(AudioBookChapter)
+class AudioBookChapterAdmin(admin.ModelAdmin):
+    list_display = ('title', 'book', 'chapter_number')
+
+@admin.register(AudioPlay)
+class AudioPlayAdmin(admin.ModelAdmin):
+    list_display = ('title', 'director', 'created_at')
+
+@admin.register(AudioPlayAct)
+class AudioPlayActAdmin(admin.ModelAdmin):
+    list_display = ('title', 'play', 'act_number')
+
+@admin.register(Poem)
+class PoemAdmin(admin.ModelAdmin):
+    list_display = ('title', 'poet')
 
 @admin.register(Playlist)
 class PlaylistAdmin(admin.ModelAdmin):
